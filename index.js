74ed7e4c8b6b567bd33e99ab9fb8ac137022831a
Isaiah <isaiah.adams7895@gmail.com>
2019-07-03 18:40:59 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+ 
+
+app.set("view engine", "ejs"); 
+
+app.use(express.static('./public'));
+
+app.get('/home', function (req, res) {
+  res.render('homecd ..');
+});
+
+ 
+app.listen(3000, function(err){
+    if (err)
+        console.log(err)
+    console.log('Server is live on port 3000')
+})
